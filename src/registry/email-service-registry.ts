90b96fda97d6a515e11220df43e874773111fc5c
Jessica Mulein <jessica@digitaldefiance.org>
2025-11-12 11:45:27 -0800

    v2.1.63

--- src/registry/email-service-registry.ts
+++ src/registry/email-service-registry.ts
@@ -19,6 +19,35 @@ class EmailServiceRegistry {
     }
     return this.service;
   }
+
+  /**
+   * Check if any email service is registered
+   */
+  hasService(): boolean {
+    return this.service !== undefined;
+  }
+
+  /**
+   * Check if the registered service is an instance of a specific class
+   * @param serviceClass The class/constructor to check against
+   * @returns true if the registered service is an instance of the given class
+   * @example
+   * emailServiceRegistry.isServiceType(EmailService) // true if EmailService is registered
+   * emailServiceRegistry.isServiceType(DummyEmailService) // true if DummyEmailService is registered
+   */
+  isServiceType<T extends IEmailService>(
+    serviceClass: new (...args: any[]) => T,
+  ): boolean {
+    return this.service !== undefined && this.service instanceof serviceClass;
+  }
+
+  /**
+   * Get the constructor name of the registered service
+   * @returns The name of the service class, or undefined if no service is registered
+   */
+  getServiceTypeName(): string | undefined {
+    return this.service?.constructor.name;
+  }
 }
 
 export const emailServiceRegistry = new EmailServiceRegistry();
