5414f17dc0fb019327734d40ce237ae8ddab9cef
Jessica Mulein <jessica@digitaldefiance.org>
2026-02-07 09:26:05 -0800

    lint

--- src/interfaces/api-message-response.ts
+++ src/interfaces/api-message-response.ts
@@ -1,4 +1,3 @@
-
-import type { IApiMessageResponse as IApiMessageResponse_ } from "@digitaldefiance/suite-core-lib";
+import type { IApiMessageResponse as IApiMessageResponse_ } from '@digitaldefiance/suite-core-lib';
 // re-export for backwards compatibility
 export type { IApiMessageResponse_ as IApiMessageResponse };
